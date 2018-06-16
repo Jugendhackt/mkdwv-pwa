29704cbcf7bc202b1d77162c8284569986e3c2fc
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-16 17:41:43 +0200

    Use OpenStreetMap tile server

--- static/js/init.js
+++ static/js/init.js
@@ -15,7 +15,7 @@ function initMap() {
   var mymap = L.map('mapid').setView([data.lat, data.lng], 13);
 
 
-  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
