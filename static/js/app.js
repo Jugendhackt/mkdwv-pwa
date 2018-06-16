376a1f30b2d79115db8a70d189d8104881641d90
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-16 15:03:07 +0200

    Imp

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,63 @@
+
+// Settings
+const DEBUG = true
+
+
+const BASE_URL = "http://localhost"
+const ENDPOINTS = {
+  'trash_upload': {
+    'method': 'POST',
+    'uri': `${BASE_URL}/trash`
+  },
+  'trash_fetch': {
+    'method': 'GET',
+    'uri': `${BASE_URL}/trash`
+  },
+  'trashcans_fetch': {
+    'method': 'GET',
+    'uri': `${BASE_URL}/trash`
+  }
+};
+
+// Endpoint list
+/*
+let UPLOAD_ENDPOINT = {}
+let TRASHCANS_ENDPOINT = {}
+let TRASH_ENDPOINT = {}
+*/
+
+const UPLOAD_ENDPOINT    = ENDPOINTS.trash_upload // Upload endpoint
+const TRASHCANS_ENDPOINT = ENDPOINTS.trashcans_fetch // Trashcans endpoint
+const TRASH_ENDPOINT     = ENDPOINTS.trash_fetch // Trash endpoint
+
+
+
+
+
 function webtest() {
   return navigator.onLine;
+}
+
+
+
+
+debugSequence()
+
+
+
+
+
+
+
+// DEBUG LOGGER
+function debugSequence() {
+  if(DEBUG) {
+    // Log endpoints
+    console.log(`Upload endpoint:`)
+    console.log(UPLOAD_ENDPOINT)
+    console.log(`Trashcans endpoint:`)
+    console.log(TRASHCANS_ENDPOINT)
+    console.log(`Trash endpoint:`)
+    console.log(TRASH_ENDPOINT)
+  }
 }
