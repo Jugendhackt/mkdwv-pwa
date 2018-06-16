0fffab9f9ef2a0739cb2cddeed95baea4425dc31
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-16 17:09:58 +0200

    Outsource initMap

--- static/js/init.js
+++ static/js/init.js
@@ -6,3 +6,41 @@ if(!webtestResult) {
   console.error("Webtest failed! Response: " + webtestResult);
   //alert("You are not connected to the internet!");
 } else console.log("Webtest success!");
+
+function initMap() {
+  var data = getPointData(1)
+  if(data == false) {
+    alert("Data could not load!");
+  }
+  var mymap = L.map('mapid').setView([data.lat, data.lng], 13);
+
+
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+      '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+      'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+  }).addTo(mymap);
+
+  function onLocationFound(e) {
+    var radius = e.accuracy / 2; //vorher 2
+
+    L.marker(e.latlng).addTo(mymap)
+      .bindPopup("Standort auf " + radius + "m genau.").openPopup();
+
+    L.circle(e.latlng, radius).addTo(mymap);
+  }
+
+  function onLocationError(e) {
+    alert(e.message);
+  }
+
+  mymap.on('locationfound', onLocationFound);
+  mymap.on('locationerror', onLocationError);
+
+  mymap.locate({setView: true, maxZoom: 16});
+
+  setMarkersFromLocations(mymap);
+
+}
