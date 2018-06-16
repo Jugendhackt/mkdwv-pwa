4df1a960e3e370d6a41c02790762d5119865763c
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-16 13:36:57 +0200

    Remove alert

--- static/js/init.js
+++ static/js/init.js
@@ -4,5 +4,5 @@ const webtestResult = webtest();
 
 if(!webtestResult) {
   console.error("Webtest failure! Not connected!");
-  alert("You are not connected to the internet!");
+  //alert("You are not connected to the internet!");
 } else console.log("Webtest success!");
