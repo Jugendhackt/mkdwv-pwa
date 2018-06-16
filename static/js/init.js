42ecc5d601e06540f02d68c271969c68faa9e035
Bjarn Klein <bjarne-kleink@gmx.de>
2018-06-16 21:19:27 +0200

    onclick alert

--- static/js/init.js
+++ static/js/init.js
@@ -35,6 +35,7 @@ function initMap() {
     alert(e.message);
   }
 
+  mymap.on('click', (e) => {alert(e)});
   mymap.on('locationfound', (e) => {updateLocation(e, mymap);});
   mymap.on('locationerror', onLocationError);
 
