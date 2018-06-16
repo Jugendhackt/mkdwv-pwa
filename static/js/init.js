43189eecc82a494b97da16f9d9e2962e5a3a6d6e
SchokoTets <moritzleni@gmail.com>
2018-06-16 17:34:29 +0200

    externalized updatePosition()

--- static/js/init.js
+++ static/js/init.js
@@ -1,11 +1,18 @@
-
 console.log("Loading webtest...");
 const webtestResult = webtest();
 
 if(!webtestResult) {
   console.error("Webtest failed! Response: " + webtestResult);
-  //alert("You are not connected to the internet!");
 } else console.log("Webtest success!");
+
+function updateLocation(e, map) {
+  var radius = e.accuracy / 2;
+
+  L.marker(e.latlng).addTo(map)
+    .bindPopup("Standort auf " + Math.round(radius*10)/10 + "m genau").openPopup();
+
+  L.circle(e.latlng, radius).addTo(map);
+}
 
 function initMap() {
   var data = getPointData(1)
@@ -14,7 +21,6 @@ function initMap() {
   }
   var mymap = L.map('mapid').setView([data.lat, data.lng], 13);
 
-
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
@@ -22,25 +28,15 @@ function initMap() {
       'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     id: 'mapbox.streets'
   }).addTo(mymap);
-
-  function onLocationFound(e) {
-    var radius = e.accuracy / 2; //vorher 2
-
-    L.marker(e.latlng).addTo(mymap)
-      .bindPopup("Standort auf " + Math.round(radius*10)/10 + "m genau").openPopup();
-
-    L.circle(e.latlng, radius).addTo(mymap);
-  }
-
+  
   function onLocationError(e) {
     alert(e.message);
   }
 
-  mymap.on('locationfound', onLocationFound);
+  mymap.on('locationfound', (e) => {updateLocation(e, mymap);});
   mymap.on('locationerror', onLocationError);
 
   mymap.locate({setView: true, maxZoom: 16});
 
   setMarkersFromLocations(mymap);
-
 }
