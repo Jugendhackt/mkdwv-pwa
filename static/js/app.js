a7c5b59210271d9d1cd2e33b2f83dbd70bf9b947
MagicMarvMan <24806960+MagicMarvMan@users.noreply.github.com>
2018-06-16 13:44:20 +0200

    Fix

--- static/js/app.js
+++ static/js/app.js
@@ -1,23 +1,3 @@
 function webtest() {
-  var request = new XMLHttpRequest();
-  request.open('GET', 'https://api.webtest.net/', true);
-
-  request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
-      var resp = request.responseText;
-      if(resp == 'OK') {
-        return true
-      } else {
-        return false
-      }
-    } else {
-      return false
-    }
-  };
-
-  request.onerror = function() {
-    return false
-  };
-
-  request.send();
+  return navigator.onLine;
 }
