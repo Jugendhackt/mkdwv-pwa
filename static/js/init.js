92dcf5ebc2126a85a512cf0f213287cddc4dc1d0
Bjarn Klein <bjarne-kleink@gmx.de>
2018-06-17 11:46:07 +0200

    edited index

--- static/js/init.js
+++ static/js/init.js
@@ -19,7 +19,7 @@ function updateLocation(e, map) {
 function initMap() {
   mymap = L.map('mapid').setView([50.104278, 8.675969], 13);
 
-  L.tileLayer(TILE_URLS.default, {
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 18,
     attribution: 'Implementation: <a href="https://github.com/jens1o">jens1o</a> | Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
@@ -30,7 +30,7 @@ function initMap() {
   function onLocationError(e) {
     alert(e.message);
   }
-
+  
   function setMarker(e){
       var lat = e.latlng.lat;
       var lng = e.latlng.lng;
@@ -38,14 +38,30 @@ function initMap() {
        marker.bindPopup(lat + " " + lng).openPopup();
   }
 
+  function editmarker(){
+    //Methode: Popup öffnen
+        var msg = document.querySelector("#description").value;
+	  markerbindPopup(msg).openPopup;
+	//markerybindPopup(textfeld.getText, lat + " " + lng );
+  }
+
+  
   mymap.on('click', setMarker);
   mymap.on('locationfound', (e) => {updateLocation(e, mymap);});
   mymap.on('locationerror', onLocationError);
-
+  marker.on('onclick', displayAdd);
   mymap.locate({setView: true, maxZoom: 16});
+
+  L.Routing.control({
+	    waypoints: [
+		        L.latLng(57.74, 11.94),
+		        L.latLng(57.6792, 11.949)
+		      ]
+  }).addTo(mymap);
 
 navigator.geolocation.getCurrentPosition(position =>  {
   console.log(position);
   populateByTrashcans(position.coords.latitude, position.coords.longitude, mymap);
 })
 }
+
