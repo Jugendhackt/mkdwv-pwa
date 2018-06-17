65a33ed7638805621733c1bcd229e57cc6c07288
SchokoTets <moritzleni@gmail.com>
2018-06-17 09:14:39 +0200

    added multiple modal divs

--- static/js/modal.js
+++ static/js/modal.js
@@ -1,7 +1,7 @@
 var modal = document.getElementsByClassName("modal")[0];
 
 // Get the button that opens the modal
-var btn = document.getElementsByClassName("headerButton")[0];
+var btn = document.getElementById("butAdd")[0];
 
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
@@ -9,7 +9,7 @@ var span = document.getElementsByClassName("close")[0];
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
     modal.style.display = "block";
-    document.getElementById("content").innerHTML = "<h2>Hey!</h2>";
+    document.getElementById("content-add").innerHTML = "<h2>Hey!</h2>";
 }
 
 // When the user clicks on <span> (x), close the modal
