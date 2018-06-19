774d19c327ecd195c145e4cfdeae708ba28d39da
SchokoTets <moritzleni@gmail.com>
2018-06-19 20:41:49 +0200

    fixed marker popup position

--- static/js/app.js
+++ static/js/app.js
@@ -140,7 +140,7 @@ function getTrashMapIcon() {
 
     iconSize:     [32, 32], // size of the icon
     iconAnchor:   [16, 16], // point of the icon which will correspond to marker's location
-    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+    popupAnchor:  [0, -18] // point from which the popup should open relative to the iconAnchor
   });
   return trashIcon;
 }
