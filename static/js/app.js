af45ff4ef08fd66c2ea137323aaac573c335c9e0
SchokoTets <moritzleni@gmail.com>
2018-07-03 19:22:47 +0200

    added semicolons in app.js

--- static/js/app.js
+++ static/js/app.js
@@ -10,7 +10,7 @@ const TILE_URLS = {
   "googleterrain": "http://mt.google.com/vt/lyrs=p&x={x}&y={y}&z={z}",
   "googlealteredroad": "http://mt.google.com/vt/lyrs=r&x={x}&y={y}&z={z}",
   "googleterrainonly": "http://mt.google.com/vt/lyrs=t&x={x}&y={y}&z={z}"
-}
+};
 
 
 const BASE_URL = "https://kfdw.herokuapp.com"
@@ -130,8 +130,8 @@ function setMarker(id, map) {
     const data = getPointData(id);
     var marker = L.marker([data.lat,data.lng], {icon: trashcanIcon}).addTo(map);
 		var popup = marker.bindPopup(data.content);
-		popup.on("popupopen", () => {marker.setIcon(trashcanSIcon)})
-		popup.on("popupclose", () => {marker.setIcon(trashcanIcon)})
+		popup.on("popupopen", () => {marker.setIcon(trashcanSIcon)});
+		popup.on("popupclose", () => {marker.setIcon(trashcanIcon)});
 		popup.openPopup();
   }
 }
@@ -148,21 +148,21 @@ function populateByTrashcans(lat, lng, map) {
   function reqListener (respText) {
     const json = JSON.parse(respText);
     for(var item of json) {
-      let subdata = JSON.parse(item.data)
-      item.subdata = subdata
-      let html = `<b>ID:</b> ${item.trashBinID}<br>`
+      let subdata = JSON.parse(item.data);
+      item.subdata = subdata;
+      let html = `<b>ID:</b> ${item.trashBinID}<br>`;
       var loc = {
         'lat': item.latitude,
         'lng': item.longitude
       }
       if(item.distance_in_m) {
-        const newDistance = (Math.round(item.distance_in_m * 10)/10).toString().replace('.', ',')
-        html += `<b>${TRANSLATION_DE.distance["_"]}:</b> <b>${newDistance} m</b>`
+        const newDistance = (Math.round(item.distance_in_m * 10)/10).toString().replace('.', ',');
+        html += `<b>${TRANSLATION_DE.distance["_"]}:</b> <b>${newDistance} m</b>`;
       }	
       var tags = ["vending", "payment:none","fee","highway","indoor","waste","_lastcheck","_level","tourism","tunnel","_operator","_name"];
 
       for(var tag of tags) {
-        let numerical = tag.charAt(0)=='_'
+        let numerical = tag.charAt(0)=='_';
 				if(numerical) tag = tag.substr(1);
         if(item.subdata[tag]) {
           html += `<br><b>${TRANSLATION_DE[tag]["_"]}:</b> `;
@@ -175,8 +175,8 @@ function populateByTrashcans(lat, lng, map) {
 
         }
       }
-	    html += `<br><button type="button" onclick="" id="show_nearest_trashcan">Anzeigen</button>  <button>Route berechnen</button>`
-      loc.content = html
+	    html += `<br><button type="button" onclick="" id="show_nearest_trashcan">Anzeigen</button>  <button>Route berechnen</button>`;
+      loc.content = html;
       LOCATIONS.push(loc);
     }
     document.getElementById("search-output").innerHTML = JSON.stringify(LOCATIONS, null, 4);
@@ -188,7 +188,7 @@ function populateByTrashcans(lat, lng, map) {
   function stopLoading(){document.getElementsByClassName("loader")[0].style.display = "none";}
   var req = new XMLHttpRequest();
   const requestUrl = `${TRASHCANS_ENDPOINT.uri}?position=${lat},${lng}`;
-  console.log(`Sending request to ${requestUrl}`)
+  console.log(`Sending request to ${requestUrl}`);
   req.open("GET", requestUrl, true);
   req.onload = () => {reqListener(req.responseText);stopLoading();};
   req.onerror = () => {console.error(req.statusText);stopLoading();};
